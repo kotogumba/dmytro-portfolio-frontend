a06acd50dd532e73e74fe00917d32d5a190454e8
Dmytro Plechun <ifunnd@gmail.com>
2023-01-09 00:12:16 +0200

    tech stack

--- src/components/Tech_stack.jsx
+++ src/components/Tech_stack.jsx
@@ -3,9 +3,12 @@ import tech_img from '../assets/tech.png'
 
 function Tech_stack() {
   return (
-    <div className='tech-stack rounded-xl p-3 mt-3 mb-3 dark:bg-neutral-800'>
-      <p className='text-2xl dark:text-gray-400 mb-3'>Tech Stack</p>
-      <img src={tech_img} alt='tech stack' className='tech-img'/>
+    <div className='tech-stack rounded-xl p-3 mt-3 mb-3 dark:bg-neutral-800 '>
+      <p className='text-2xl dark:text-gray-400 mb-3 '>Tech Stack</p>
+      <div className="flex justify-content-center">
+
+        <img src={tech_img} alt='tech stack' className='tech-img w-5/6'/>
+      </div>
     </div>
   )
 }
